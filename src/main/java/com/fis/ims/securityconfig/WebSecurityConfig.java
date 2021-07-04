049ca2a314ad04a4792d162602b17649a417a4c0
prakharfanse <prakharprakharprakhar25@gmail.com>
2021-07-04 22:07:48 +0530

    Still 403 unauthorised

--- src/main/java/com/fis/ims/securityconfig/WebSecurityConfig.java
+++ src/main/java/com/fis/ims/securityconfig/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.fis.ims.securityconfig;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,8 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
+	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.inMemoryAuthentication().withUser("admin").password("admin");
+	}
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable();
+		http.httpBasic().and().authorizeRequests().antMatchers("/api/ims/**").hasAnyRole().and().csrf().disable()
+				.formLogin().disable();
 	}
 
 }
